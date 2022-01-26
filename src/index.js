d11974422db3c0f87d844f90f102d43a81d66f60
Denwa <denwa799@yandex.ru>
2022-01-26 14:47:13 +0300

    Add redux to quiz list page

--- src/index.js
+++ src/index.js
@@ -4,14 +4,33 @@ import './index.css';
 import {BrowserRouter} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {compose, createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux'
+import rootReducer from "./store/reducers/rootReducer";
+import thunk from 'redux-thunk'
+
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        }) : compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
